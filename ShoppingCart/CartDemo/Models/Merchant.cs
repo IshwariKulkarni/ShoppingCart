2bd6c5cf0767a95cc81ce05548fb1fc7ce0929e5
nishika <waghmarenishika@gmail.com>
2023-04-04 22:40:20 +0530

    merchat added

--- ShoppingCart/CartDemo/Models/Merchant.cs
+++ ShoppingCart/CartDemo/Models/Merchant.cs
@@ -5,29 +5,3 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
-namespace CartDemo.Models
-{
-    public class Merchant { 
-
-        [Key]
-        public string MerchantId{get; set;}
-
-        [Required(ErrorMessage = "Required")]
-        public string MerchantName { get; set; }
-
-        [Required(ErrorMessage = "Required")]
-
-        
-        public string SellingPrice { get; set; }
-
-        [ForeignKey("Product")]
-        public string ProductId { get; set; }
-
-        
-        //public string ProductName { get; set; }
-    
-    public Product Product { get; set; }
-
-
-}
-}
